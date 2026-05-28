44aa6da03477c05b20236b7024acb0c6cbb40b01
Soham Santosh Deshpande <soham.deshpande100904@gmail.com>
2026-05-28 15:37:41 +0530

    updated

--- Day 9/capgemini1.py
+++ Day 9/capgemini1.py
@@ -1,1 +1,27 @@
-sem=int(input("Enter the no. of sem"))
+def main():
+    try:
+        sem = int(input("Enter no of semester:\n"))
+        max_marks = []
+
+        for i in range(1, sem + 1):
+            subjects = int(input(f"Enter no of subjects in {i} semester:\n"))
+            print(f"Marks obtained in semester {i}:")
+
+            marks = []
+            for j in range(subjects):
+                mark = int(input())
+                if mark < 0 or mark > 100:
+                    print("You have entered invalid mark.")
+                    return
+                marks.append(mark)
+
+            max_marks.append(max(marks))
+
+        for i in range(sem):
+            print(f"Maximum mark in {i+1} semester:{max_marks[i]}")
+
+    except ValueError:
+        print("You have entered invalid mark.")
+
+if __name__ == "__main__":
+    main()
